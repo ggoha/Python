9c7eb3eb6c9d4a0060ff1b1c08e2c646dd536268
ggoha <ryznikov@mail.ru>
2014-04-21 20:16:41 +0400

    add QTping

--- ping.py
+++ ping.py
@@ -1,74 +1,91 @@
-import queue, threading, subprocess, sys, socket, argparse
+import sys, threading, subprocess
+from PySide import QtCore, QtGui
+
+class Pinger(QtGui.QWidget):
+
+	def __init__(self, parent=None):
+		super(Pinger, self).__init__(parent)
+
+		IPLabel = QtGui.QLabel("IP:")
+		self.IPaddress_text = QtGui.QTextEdit()
+
+		LogLabel = QtGui.QLabel("LOG:")
+		self.Log_text = QtGui.QTextEdit()
+		self.Log_text.setReadOnly(True)
+
+		self.PingButton = QtGui.QPushButton("PING")
+		self.PingButton.show()
+
+		self.PingButton.clicked.connect(self.Ping)
+
+		self.count_threadSpinBox = QtGui.QSpinBox()
+		self.count_threadSpinBox.setRange(1, 99)
+		self.count_threadSpinBox.setSingleStep(1)
+		self.count_threadSpinBox.setValue(10)
+
+		self.count_threadSpinBox.valueChanged[int].connect(self.changed)
+		self.count_thread=10
+
+		mainLayout = QtGui.QGridLayout()
+		mainLayout.addWidget(IPLabel, 0, 0)
+		mainLayout.addWidget(self.IPaddress_text, 0, 1)
+		mainLayout.addWidget(LogLabel, 1, 0)
+		mainLayout.addWidget(self.Log_text, 1, 1)
+		mainLayout.addWidget(self.PingButton, 2, 1)
+		mainLayout.addWidget(self.count_threadSpinBox, 2, 0)
+
+		self.setLayout(mainLayout)
+		self.setWindowTitle("Ping")
+
+	def changed(self, value):
+		self.count_thread=value
+
+	def Ping(self):
+		self.massip=list(self.IPaddress_text.toPlainText().splitlines())
+		self.Log_text.setText(ThreadPing(self.massip, self.count_thread).run())
+
+class ThreadPing():
+	def __init__(self, ips, NumberThreads):
+		import queue
+		self.NUMBER_THREADS=NumberThreads
+		self.ip=queue.Queue()
+
+		for x in ips:
+			self.ip.put(x)
+        
+		self.threads= []
+	def run(self):
+		global res
+		res=[]
+		for x in range(self.NUMBER_THREADS):
+			res.append('')
+			self.threads.append(MyPing(self.ip, x))
+			self.threads[-1].start()
+		for x in range(self.NUMBER_THREADS):
+			self.ip.join()
+		print (res)
+		return (''.join(res))
 
 class MyPing(threading.Thread):
-    '''
-    Класс который будет извлекать IP из очереди и проверять их
-    '''
-    def __init__(self, ip, output):
-        #Инициализируем родителя
-        super(MyPing,self).__init__()
-        self.output=output
-        self.ip=ip
-    def run (self):
-        '''
-        проверка pinga
-        '''
-            
-        while not self.ip.empty():
-            CurrentIp=self.ip.get()
-            result = subprocess.call("ping -s 1 %s" % CurrentIp, 
-                                  shell=True, 
-                                  stdout=subprocess.PIPE, 
+	def __init__(self, ip, x):
+		super(MyPing,self).__init__()
+		self.ip=ip
+		self.x=x
+	def run (self):
+		while not self.ip.empty():
+			CurrentIp=self.ip.get()
+			result = subprocess.call("ping -c 1 %s" % CurrentIp,
+                                  shell=True,
+                                  stdout=subprocess.PIPE,
                                   stderr=subprocess.STDOUT)
-            if result==0:
-                self.output.write(CurrentIp+'\n')
-                
-            self.ip.task_done()
+			if result==0:
+				res[self.x]+='''<font color="green">%s</font><br>''' %CurrentIp
+			else:
+				res[self.x]+='''<font color="red">%s</font><br>''' %CurrentIp
+			self.ip.task_done()
             
-class ThreadPing():
-    '''
-    Обработчик потоков
-    '''
-    def __init__(self, ips, output,  NumberThreads):
-        self.NUMBER_THREADS=NumberThreads
-        #Создаем очередь IP
-        self.ip=queue.Queue()
-        
-        #Обрабатывам исключения с открытием файла
-        try:
-            self.output=open(output, 'w')
-        except OSError:
-            print("OSError")
-            exit()
-        
-        #Заполняем очередь
-        for x in ips:
-            self.ip.put(x)
-        
-        self.threads= []
-    def run(self):
-        for x in range(self.NUMBER_THREADS):
-            self.threads.append(MyPing(self.ip, self.output))
-            self.threads[-1].start()
-        #Ждем опустошения очереди
-        self.ip.join()
-        self.output.close()
-
-#Парсинг
-parser = argparse.ArgumentParser()
-parser.add_argument('-n', action='store', type=int, dest='n', default=10, help='Count threads')
-parser.add_argument('-o', action='store', dest='o', default='ping.log', help='Output file')
-parser.add_argument('-ip', action='store', help='Mass of IPaddress', nargs='+')
-args = parser.parse_args(sys.argv[1:-1])
-
-#Проверка на непустоту
-if args.ip is None:
-    exit()
-#Проверка на корректность IP
-for x in args.ip:
-    try:
-        socket.inet_aton(x)
-    except socket.error: 
-        args.ip.remove(x)
-
-ThreadPing(args.ip, args.o, args.n).run()
+if __name__ == '__main__':
+	app = QtGui.QApplication(sys.argv)
+	pinger = Pinger()
+	pinger.show()
+	sys.exit(app.exec_())
